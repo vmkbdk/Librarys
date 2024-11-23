a2fe09497271a0351323c8123e99e7a001f0aef4
cbvdjgei5610 <ruby79342@gmail.com>
2024-11-23 06:54:29 -0800

    Add class Book& clacc Library

--- Library/Program.cs
+++ Library/Program.cs
@@ -6,10 +6,52 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    internal class Program
+    class Book
     {
-        static void Main(string[] args)
+        public string Titel { get; set; }
+        public string Author { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+    class Library
+    {
+        List<Book> Books = new List<Book>();
+        public void AddBook(Book Book)
         {
+            try
+            {
+                if (string.IsNullOrEmpty(Book.Titel) || string.IsNullOrEmpty(Book.Author))
+                {
+                    Console.WriteLine("Book titel and author must not be empty.");
+                }
+                Books.Add(Book);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + "An error occurred while adding the book.");
+            }
+        }
+        public List<Book> SerchByTitel(string titel)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(titel))
+                {
+                    Console.WriteLine("You must enter the search title:");
+                }
+                return Books.Where(B => B.Titel.Contains(titel, stringComparison.OrdinallgnoreCase)).ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message+"An error occurred while searching");
+                return new List<Book>();
+            }
+        }
+    }
+    class Programe
+    {
+        static void Main(string[] arges)
+        {
+
         }
     }
 }
